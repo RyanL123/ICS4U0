70380671e82d2026529870c79a420a10bd0ee911
Ryan Li <ryan.lichangrui@gmail.com>
2020-10-02 20:21:14 -0400

    hero implementation + setup for emerging tech

--- Emerging Tech/gatsby-config.js
+++ Emerging Tech/gatsby-config.js
@@ -1,4 +1,17 @@
+const path = require(`path`)
+
 module.exports = {
-  pathPrefix: "/ICS4U0",
-  plugins: [],
+    pathPrefix: "/ICS4U0",
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: path.join(__dirname, `src`, `images`),
+            },
+        },
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-chakra-ui`,
+    ],
 }
